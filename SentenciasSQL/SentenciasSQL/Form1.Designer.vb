c06467b23046503be6c5a6af8da59dbf8943b5ec
Lenin Lucas <63570798+leninjo@users.noreply.github.com>
2020-05-08 12:48:28 -0600

    Add files via upload

---
+++ SentenciasSQL/SentenciasSQL/Form1.Designer.vb
@@ -0,0 +1,115 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class LectorSQL
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(LectorSQL))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.cadena = New System.Windows.Forms.TextBox()
+        Me.lista = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.CornflowerBlue
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.Button1.Font = New System.Drawing.Font("Livvic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(88, 115)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(134, 37)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "ANALIZAR"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.LightGreen
+        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.Button2.Font = New System.Drawing.Font("Livvic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(274, 115)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(132, 37)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "LIMPIAR"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Livvic", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.White
+        Me.Label1.Location = New System.Drawing.Point(38, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(414, 31)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Ingresa la sentencia SQL que seas analizar."
+        '
+        'cadena
+        '
+        Me.cadena.BackColor = System.Drawing.Color.MediumTurquoise
+        Me.cadena.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.cadena.Font = New System.Drawing.Font("Livvic", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cadena.ForeColor = System.Drawing.SystemColors.InactiveCaptionText
+        Me.cadena.Location = New System.Drawing.Point(12, 64)
+        Me.cadena.Name = "cadena"
+        Me.cadena.Size = New System.Drawing.Size(460, 24)
+        Me.cadena.TabIndex = 3
+        '
+        'lista
+        '
+        Me.lista.BackColor = System.Drawing.Color.MediumTurquoise
+        Me.lista.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.lista.Font = New System.Drawing.Font("Livvic", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lista.FormattingEnabled = True
+        Me.lista.ItemHeight = 27
+        Me.lista.Location = New System.Drawing.Point(26, 175)
+        Me.lista.Name = "lista"
+        Me.lista.Size = New System.Drawing.Size(435, 270)
+        Me.lista.TabIndex = 6
+        '
+        'LectorSQL
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.DarkCyan
+        Me.ClientSize = New System.Drawing.Size(484, 461)
+        Me.Controls.Add(Me.lista)
+        Me.Controls.Add(Me.cadena)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "LectorSQL"
+        Me.Text = "LectorSQL"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents cadena As TextBox
+    Friend WithEvents lista As ListBox
+End Class
---
+++ SentenciasSQL/SentenciasSQL/Form1.vb
@@ -0,0 +1,73 @@
+﻿Imports System.ComponentModel
+
+Public Class LectorSQL
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles cadena.TextChanged
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+    End Sub
+
+    Private Sub analizar(sender As Object, e As MouseEventArgs) Handles Button1.MouseClick
+
+        Dim reservadas() As String = {"select", "from", "where"}
+        Dim identificadores() As String = {"alumnos", "libros", "profesores", "usuarios", "personas"}
+        Dim operadores() As String = {"*", "(", ")", "=", ".", ",", "+"}
+
+        Dim text As String = cadena.Text
+        Dim sentencia() As String = Split(text)
+
+
+        For Each palabra As String In sentencia
+            ' verifica si la palabra es reservada ¿
+            For Each palabra2 As String In reservadas
+                If palabra = palabra2 Then
+                    lista.Items.Add("» Reservada: " + palabra)
+                End If
+            Next
+
+            For Each palabra2 As String In identificadores
+                If palabra = palabra2 Then
+                    lista.Items.Add("» Identificador: " + palabra)
+                End If
+            Next
+
+            ' verifica si la palabra es un operador '
+            For Each palabra2 As String In operadores
+                If palabra = palabra2 Then
+                    lista.Items.Add("» Operador: " + palabra)
+                End If
+            Next
+
+            ' verifica si la cadena es número '
+            For Each palabra2 As String In sentencia
+                If IsNumeric(palabra) Then
+                    lista.Items.Add("» Número: " + palabra)
+                End If
+            Next
+
+            ' verifica si la palabra es una cadena '     
+            For Each palabra2 As Char In palabra
+                If palabra2 = "'" Then
+                    lista.Items.Add("» Cádena: " + palabra)
+                    Exit For
+                End If
+            Next
+        Next
+
+    End Sub
+
+    Private Sub lista_SelectedIndexChanged(sender As Object, e As EventArgs) Handles lista.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        cadena.Clear()
+        lista.Items.Clear()
+    End Sub
+End Class
